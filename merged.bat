2dd4a0f101977fa55258af1adf460d7a40785804
kuko010 <kukouwu@proton.me>
2026-03-04 21:45:41 +0100

    pre-merge

---
+++ merged.bat
@@ -0,0 +1,11 @@
+git checkout bolt
+git add .
+git commit -m "pre-merge"
+git push
+git stash push -u -m "stash untracked before logicsim merge"
+git checkout main
+git checkout bolt -- data/logicsim
+git add data/logicsim
+git commit -m "Override logicsim folder from bolt branch"
+echo "Done! logicsim is now updated from bolt."
+git checkout bolt
